194a2a9b934ead7ffe3a0fb003236733fa6b5824
Atik Hassan <atikh01001@gmail.com>
2025-05-24 01:53:59 +0600

    banner modified

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -17,7 +17,8 @@ const slides = [
     id: 2,
     title: "Secure Transactions",
     description: "Your information is fully protected and encrypted.",
-    image: "https://i.ibb.co.com/gMBC119Z/phishing-illustration-hacker-stealing-credit-card-from-app-cybercrime-1150-37365.jpg",
+    image:
+      "https://i.ibb.co.com/gMBC119Z/phishing-illustration-hacker-stealing-credit-card-from-app-cybercrime-1150-37365.jpg",
   },
   {
     id: 3,
@@ -29,13 +30,15 @@ const slides = [
     id: 4,
     title: "Electricity Bill",
     description: "Easily pay your electricity bills from anywhere.",
-    image: "https://i.ibb.co.com/PszGGjb0/planning-utility-costs-monthly-budget-197463-873.jpg",
+    image:
+      "https://i.ibb.co.com/PszGGjb0/planning-utility-costs-monthly-budget-197463-873.jpg",
   },
   {
     id: 5,
     title: "Gas Bill",
     description: "Pay your gas bill securely and without hassle.",
-    image: "https://i.ibb.co.com/dwLJJLmy/counting-euro-banknotes-near-burning-gas-stove-506452-17566.jpg",
+    image:
+      "https://i.ibb.co.com/dwLJJLmy/counting-euro-banknotes-near-burning-gas-stove-506452-17566.jpg",
   },
   {
     id: 6,
@@ -54,7 +57,8 @@ const slides = [
     id: 8,
     title: "Credit Card Bill",
     description: "Clear your credit card dues with a few simple taps.",
-    image: "https://i.ibb.co.com/0NY44cz/car-model-calculator-bank-statement-credit-card-wooden-table-43314-2069.jpg",
+    image:
+      "https://i.ibb.co.com/0NY44cz/car-model-calculator-bank-statement-credit-card-wooden-table-43314-2069.jpg",
   },
   {
     id: 9,
@@ -87,8 +91,12 @@ const Banner = () => {
               }}
             >
               <div className=" flex flex-col justify-center items-center bg-opacity-50  gap-3 rounded-lg ">
-                <h2 className="text-4xl font-bold text-blue-700">{slide.title}</h2>
-                <p className="mt-4 text-lg text-blue-500 font-medium">{slide.description}</p>
+                <h2 className="text-4xl font-bold text-blue-700">
+                  {slide.title}
+                </h2>
+                <p className="mt-4 text-lg text-blue-500 font-medium">
+                  {slide.description}
+                </p>
                 <Link
                   to="/bills"
                   className="btn w-[200px] rounded-full text-xl text-blue-700 px-5 py-2"
