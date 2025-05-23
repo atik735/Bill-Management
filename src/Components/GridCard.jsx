192eb6a587ae7eb777e4dcd387d50816906b8f4f
Atik Hassan <atikh01001@gmail.com>
2025-05-24 01:58:09 +0600

    alt modified

--- src/Components/GridCard.jsx
+++ src/Components/GridCard.jsx
@@ -10,19 +10,19 @@ const GridCard = () => {
         <h2 className='text-2xl pb-5 font-bold text-center text-blue-700'>Available Payment Partners</h2>
         <div className=' grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-10 '>
             <div className='bg-white shadow p-4 card  flex items-center flex-col hover:shadow-xl transition duration-300'>
-                <img className='w-20' src={wasa} alt="" />
+                <img className='w-20' src={wasa} alt="wasa" />
                 <p className='font-bold text-xl text-blue-700'>WASA</p>
             </div>
             <div className='bg-white shadow p-4 card  flex items-center flex-col hover:shadow-xl transition duration-300'>
-                <img className='w-20' src={btcl} alt="" />
+                <img className='w-20' src={btcl} alt="btcl" />
                 <p className='font-bold text-xl text-blue-700'>BTCL</p>
             </div>
             <div className='bg-white shadow p-4 card  flex items-center flex-col hover:shadow-xl transition duration-300'>
-                <img className='w-20' src={nesco} alt="" />
+                <img className='w-20' src={nesco} alt="nesco" />
                 <p className='font-bold text-xl text-blue-700'>NESCO</p>
             </div>
             <div className='bg-white shadow p-4 card  flex items-center flex-col hover:shadow-xl transition duration-300'>
-                <img className='h-[80px]' src={desco} alt="" />
+                <img className='h-[80px]' src={desco} alt="desco" />
                 <p className='font-bold text-xl text-blue-700'>DESCO</p>
             </div>
         </div>
